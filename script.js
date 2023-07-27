fbfc8a360d8938ee04a36decb259e698409b9cc4
adrian-8901 <adrianlam230@gmail.com>
2023-07-27 22:01:13 +0800

    css update

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ window.onbeforeunload = function () {
 }
 
 document.onscroll = function() {
-    if (window.scrollY > window.innerHeight * 0.56) {
+    if (window.scrollY > window.innerHeight * 0.42) {
         const page2 = document.getElementById("page2");
         page2.style.display = "block";
         page2.style.animation = "fadeIn 5s";
         console.log("loaded2");
     }
-    if (page2.style.display === "block" && window.scrollY > 1.5*window.innerHeight) {
+    if (page2.style.display === "block" && window.scrollY > 1.34*window.innerHeight) {
         const page3 = document.getElementById("page3");
         page3.style.display = "block";
         page3.style.animation = "fadeIn 3s";
